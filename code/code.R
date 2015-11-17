a5a3d36e45950781e12611e083e883fe10f12781
Paulina Escarcia <pauEscarcia@MacBook-Pro-de-Paulina.local>
2015-11-17 01:29:07 -0600

    Code

--- code/code.R
+++ code/code.R
@@ -1,6 +1,14 @@
 getwd()
-setwd("Swirl-")
+setwd("BigData/Swirl-")
+setwd("statistics(The_easier_way_with_R)")
 library(swirlify)
 #mi leccion y mi curso 
 new_lesson("Lesson 1", "statistics(The easier way with R)")
 
+#para abrir la leccion existente
+set_lesson("/Users/pauEscarcia/BigData/Swirl-/statistics(The_easier_way_with_R)/Lesson_1/lesson.yaml") 
+
+#ayuda
+hlp() 
+
+testit()
